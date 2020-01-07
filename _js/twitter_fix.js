ff16e9114701db99f809edbcdd79c2c4d11c2ea1
Alexander Huber <alhuber1502@gmail.com>
2020-01-07 20:26:11 +0000

    minor update

--- _js/twitter_fix.js
+++ _js/twitter_fix.js
@@ -6,11 +6,13 @@ $(document).ready(function() {
         var twitterTimelineFoot = twitterFrame.contents().find(".timeline-Footer a");
         var twitterTimelineAuthor = twitterFrame.contents().find(".TweetAuthor-name");
         var twitterTimelineText = twitterFrame.contents().find(".timeline-Tweet-text");
+        var twitterTimelineTextLinks = twitterFrame.contents().find(".timeline-Tweet-text a");
 	if( twitterFrame.length && twitterTimelineHeader.length) {
 	    twitterFrame.attr("style","width:100%;");
             twitterTimelineHead.attr("style","display:none;");
 <!--            twitterTimelineHead.attr("style","padding:5px 0;"); -->
             twitterTimelineFoot.attr("style","color: #8C001A;");
+            twitterTimelineTextLinks.attr("style","color: #8C001A !important;");
             twitterTimelineAuthor.attr("style","font-weight:normal;");
             twitterTimelineText.attr("style","font-size: 14px; line-height: 20px; font-family: Lato,-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,'Helvetica Neue',Arial,sans-serif;");
             clearInterval(twitterCheck);
